7f1a83f800109d89fc4b6f52166e30222b0d32f6
Kenzhu-A <kenzhu_aguilera@dlsl.edu.ph>
2024-11-30 17:50:19 +0800

    test

---
+++ Students.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Student {
+    private:
+    int studentID;
+
+    public:
+    void displayStudentGrades(){
+
+    }
+
+    void displayStudentDeadlines(){
+
+    }
+
+    void displayStudentAttendance(){
+
+    }
+
+    void displayPaymentHistory(){
+
+    }
+
+    void displayStudentInformation(){
+
+    }
+
+    void editStudentInformation(){
+
+    }
+};
